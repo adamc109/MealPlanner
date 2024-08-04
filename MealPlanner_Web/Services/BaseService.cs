e325d96b4f2ff8cc4743b4c241dc14489355cd69
Adam <adam1092002@icloud.com>
2024-08-04 22:05:51 +0100

    Add Base Service

---
+++ MealPlanner_Web/Services/BaseService.cs
@@ -0,0 +1,44 @@
+﻿using MealPlanner_API.Models;
+using MealPlanner_Utiliy;
+using MealPlanner_Web.Models;
+using MealPlanner_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MealPlanner_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            //initialise response model
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public Task<T> SendAsync<T>(APIRequest aPIRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MealAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(aPIRequest.Url);
+                if (aPIRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(aPIRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+                switch (aPIRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ MealPlanner_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using MealPlanner_API.Models;
+using MealPlanner_Web.Models;
+
+namespace MealPlanner_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+
+        Task<T> SendAsync<T>(APIRequest aPIRequest);
+    }
+}
